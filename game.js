2301bc635d660d998b70ffc5a4a4941dab1605e5
Kokutse DJOGUENOU <kokutse@Kokutses-MacBook-Pro.local>
2022-03-07 20:16:56 -0500

    Added food element & colisin detection with snake element

--- game.js
+++ game.js
@@ -2,13 +2,16 @@
 const canvas = document.getElementById("game-board");
 const ctx = canvas.getContext("2d");
 
-let speed = 10;
+let speed = 7;
 
 let snakeHeadX = 13; 
 let snakeHeadY = 12;
 let tileCount = 25;
 let tileBlock = canvas.width / tileCount;
 let tileSize = tileBlock - 2;
+
+let foodX = 5;
+let foodY = 5;
 
 let xVelocity = 0;
 let yVelocity = 0;
@@ -17,8 +20,10 @@ let yVelocity = 0;
 function drawGame() {
 
     clearScreen();
-    changeSnakePosition();
     drawSnake();
+    changeSnakePosition();
+    drawFood();
+    checkFoodCollision();
     setTimeout(drawGame, 1000 / speed);
 }
 
@@ -31,7 +36,7 @@ function clearScreen() {
 //Snake element
 function drawSnake() {
     ctx.fillStyle = "orange";
-    ctx.fillRect(snakeHeadX * tileCount, snakeHeadY * tileCount, tileSize, tileSize);
+    ctx.fillRect(snakeHeadX * tileBlock, snakeHeadY * tileBlock, tileSize, tileSize);
   }
 
 function changeSnakePosition() {
@@ -39,30 +44,47 @@ function changeSnakePosition() {
     snakeHeadY = snakeHeadY + yVelocity;
   }
   
+// food element
+function drawFood(){
+    ctx.fillStyle = "red";
+    ctx.fillRect(foodX * tileBlock, foodY * tileBlock, tileSize, tileSize);
+}
+
+//collision detection
+function checkFoodCollision(){
+    if(foodX === snakeHeadX && foodY === snakeHeadY){
+        foodX = Math.floor(Math.random()*tileCount)
+        foodY = Math.floor(Math.random()*tileCount)
+    }
+}
 
 //Moving snake element (up, down, right & left arrow keys) 
 document.body.addEventListener("keydown", keyDown);
 function keyDown(event) {
     //up
     if (event.keyCode == 38) {
+        if(yVelocity == 1) return;
         xVelocity = 0;
         yVelocity = -1;
     }
   
     //down
     if (event.keyCode == 40) {
+        if(yVelocity == -1) return;
         xVelocity = 0;
         yVelocity = 1;
     }
   
     //left
     if (event.keyCode == 37) {
+        if(xVelocity == 1) return;
         xVelocity = -1;
         yVelocity = 0;
     }
   
     //right
     if (event.keyCode == 39) {
+        if(xVelocity == -1) return;
         xVelocity = 1;
         yVelocity = 0;
   }

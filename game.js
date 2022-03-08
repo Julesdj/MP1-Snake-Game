d3ea4a81efbadfa7bfb8e25f8ae88c4fb4acf8f2
Kokutse DJOGUENOU <kokutse@Kokutses-MacBook-Pro.local>
2022-03-07 21:26:45 -0500

    Every time the snake collides with food element, it grows

--- game.js
+++ game.js
@@ -4,11 +4,13 @@ const ctx = canvas.getContext("2d");
 
 let speed = 7;
 
-let snakeHeadX = 13; 
-let snakeHeadY = 12;
 let tileCount = 25;
-let tileBlock = canvas.width / tileCount;
+let tileBlock = canvas.width / tileCount; //using this so that the code does't break in case canvas size changes
 let tileSize = tileBlock - 2;
+let snakeHeadX = 13;
+let snakeHeadY = 12;
+const snakeParts = [];
+let tailLength = 2;
 
 let foodX = 5;
 let foodY = 5;
@@ -18,7 +20,6 @@ let yVelocity = 0;
 
 //The game loop
 function drawGame() {
-
     clearScreen();
     drawSnake();
     changeSnakePosition();
@@ -31,63 +32,93 @@ function drawGame() {
 function clearScreen() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }
+}
 
 //Snake element
 function drawSnake() {
+    ctx.fillStyle = "green";
+    for (let i = 0; i < snakeParts.length; i++) {
+        let part = snakeParts[i];
+        ctx.fillRect(
+            part.x * tileBlock,
+            part.y * tileBlock,
+            tileSize,
+            tileSize
+        );
+    }
+
+    snakeParts.push(new SnakePart(snakeHeadX, snakeHeadY)); //put an item at the end of the list next to the snake head
+    if (snakeParts.length > tailLength) {
+        snakeParts.shift(); // remove the furthet item from the snake parts if we have more than our tail size.
+    }
+
+    //snake head
     ctx.fillStyle = "orange";
-    ctx.fillRect(snakeHeadX * tileBlock, snakeHeadY * tileBlock, tileSize, tileSize);
-  }
+    ctx.fillRect(
+        snakeHeadX * tileBlock,
+        snakeHeadY * tileBlock,
+        tileSize,
+        tileSize
+    );
+}
 
 function changeSnakePosition() {
     snakeHeadX = snakeHeadX + xVelocity;
     snakeHeadY = snakeHeadY + yVelocity;
-  }
-  
-// food element
-function drawFood(){
-    ctx.fillStyle = "red";
-    ctx.fillRect(foodX * tileBlock, foodY * tileBlock, tileSize, tileSize);
 }
 
-//collision detection
-function checkFoodCollision(){
-    if(foodX === snakeHeadX && foodY === snakeHeadY){
-        foodX = Math.floor(Math.random()*tileCount)
-        foodY = Math.floor(Math.random()*tileCount)
+class SnakePart {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
 }
 
-//Moving snake element (up, down, right & left arrow keys) 
+// food element
+function drawFood() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(foodX * tileBlock, foodY * tileBlock, tileSize, tileSize);
+}
+function checkFoodCollision() {
+    if (foodX === snakeHeadX && foodY === snakeHeadY) {
+        foodX = Math.floor(Math.random() * tileCount);
+        foodY = Math.floor(Math.random() * tileCount);
+        tailLength++;
+    }
+}
+
+//collision detection
+
+//Moving snake element (up, down, right & left arrow keys)
 document.body.addEventListener("keydown", keyDown);
 function keyDown(event) {
     //up
     if (event.keyCode == 38) {
-        if(yVelocity == 1) return;
+        if (yVelocity == 1) return;
         xVelocity = 0;
         yVelocity = -1;
     }
-  
+
     //down
     if (event.keyCode == 40) {
-        if(yVelocity == -1) return;
+        if (yVelocity == -1) return;
         xVelocity = 0;
         yVelocity = 1;
     }
-  
+
     //left
     if (event.keyCode == 37) {
-        if(xVelocity == 1) return;
+        if (xVelocity == 1) return;
         xVelocity = -1;
         yVelocity = 0;
     }
-  
+
     //right
     if (event.keyCode == 39) {
-        if(xVelocity == -1) return;
+        if (xVelocity == -1) return;
         xVelocity = 1;
         yVelocity = 0;
-  }
+    }
 }
 
 drawGame();
